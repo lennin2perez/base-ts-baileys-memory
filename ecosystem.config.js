1aacfd1c1ef32bf182567d2b59f2e64a96331207
lennin2perez <lenninaperez@gmail.com>
2024-04-30 19:37:45 -0400

    corrigiendo error de ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    apps : [
-        {
-            name: "touchebuilderbot",
-            script: "./dist/app.js",
-            watch: false,
-            max_memory_restart: '900M',
-            exec_mode:"cluster",
-            instances: 1,
-            cron_restart: "59 23 * * *",
-            env: {
-                NODE_ENV: "development",
-            },
-            env_production: {
-                NODE_ENV: "production",
-            }
-        }
-    ]
-  }
+  apps : [
+      {
+          name: "touchebuilderbot",
+          script: "./dist/app.js",
+          watch: false,
+          max_memory_restart: '900M',
+          exec_mode:"cluster",
+          instances: 1,
+          cron_restart: "59 23 * * *",
+          env: {
+              NODE_ENV: "development",
+          },
+          env_production: {
+              NODE_ENV: "production",
+          }
+      }
+  ]
+};
